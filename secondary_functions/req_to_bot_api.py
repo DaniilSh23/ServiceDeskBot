dabb09b7bf106891a8171b97c934443b789c02fa
DaniilSh23 <danyasevas11@gmail.com>
2023-05-12 13:22:15 +0300

    почистил код от следов прошлого проекта 3

--- secondary_functions/req_to_bot_api.py
+++ secondary_functions/req_to_bot_api.py
@@ -35,32 +35,3 @@ async def post_for_check_user(tlg_username, tlg_id):
             else:
                 logger.warning(f'Вообще хз, что тут произошло ещё.')
                 return
-
-
-# TODO: ниже функции с прошлого проекта, позже моэно удалить
-async def post_user_data(user_data):
-    """
-    POST запрос для создания или обновления записи о пользователе в БД.
-    """
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url=USER_DATA_URL, data=user_data) as response:
-            if response.status == 200:
-                logger.success(f'Успешный POST-запрос для создания/обновления записи о пользователе бота')
-                return True
-            else:
-                logger.warning(f'POST-запрос для создания/обновления записи о пользователе бота НЕ УДАЛСЯ')
-                return False
-
-
-async def get_vpn_bot_admins():
-    """
-    GET запрос для получения списка админов бота.
-    """
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=VPN_BOT_SETTINGS_URL) as response:
-            if response.status == 200:
-                logger.success(f'Успешный GET-запрос для получения списка админов бота')
-                return await response.json()
-            else:
-                logger.warning(f'GET-запрос для получения списка админов бота НЕ УДАЛСЯ')
-                return False
